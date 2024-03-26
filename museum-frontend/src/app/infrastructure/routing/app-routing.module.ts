854ad1f3c6757847efe6fb58dce05cb4629e4965
dokma11 <vule.dok@gmail.com>
2024-03-26 22:00:19 +0100

    feat: Login and register routes addition

--- museum-frontend/src/app/infrastructure/routing/app-routing.module.ts
+++ museum-frontend/src/app/infrastructure/routing/app-routing.module.ts
@@ -1,22 +1,23 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "src/app/feature-modules/layout/home/home.component";
+import { LoginComponent } from "../auth/login/login.component";
+import { RegistrationComponent } from "../auth/registration/registration.component";
 
 const routes: Routes = [
   { 
     path: 'home', 
     component: HomeComponent 
   },
-  /*
   { 
     path: 'login', 
     component: LoginComponent 
   },
   { 
     path: 'register', 
-    component: UserRegistrationComponent 
+    component: RegistrationComponent 
   },
-*/
+
 ];
 
 @NgModule({
