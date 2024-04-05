aaf445aecb6b5b91a72d214ba7751d0ef6f11643
dokma11 <vule.dok@gmail.com>
2024-04-05 16:59:23 +0200

    feat: Add employee form initialization

---
+++ museum-frontend/src/app/feature-modules/administration/add-employee-form/add-employee-form.component.ts
@@ -0,0 +1,75 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from 'src/app/infrastructure/auth/auth.service';
+import { Registration } from 'src/app/infrastructure/auth/model/registration.model';
+
+@Component({
+  selector: 'app-add-employee-form',
+  templateUrl: './add-employee-form.component.html',
+  styleUrls: ['./add-employee-form.component.css']
+})
+export class AddEmployeeFormComponent {
+  isPasswordVisible: boolean;
+  isRepeatPasswordVisible: boolean;
+
+  constructor(private authService: AuthService){
+    this.isPasswordVisible = false;
+    this.isRepeatPasswordVisible = false;
+  }
+
+  registrationForm = new FormGroup({
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required]),
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+    repeatpassword: new FormControl('', [Validators.required]),
+    role: new FormControl('', [Validators.required]),
+  });
+
+  buttonState: string = 'idle'; 
+  focused: string = '';
+
+  addEmployee(): void {
+    const registration: Registration = {
+      firstName: this.registrationForm.value.firstName || "",
+      lastName: this.registrationForm.value.lastName || "",
+      email: this.registrationForm.value.email || "",
+      username: this.registrationForm.value.username || "",
+      password: this.registrationForm.value.password || "",
+      role: 0
+    };
+
+    console.log(registration);
+
+    if (this.registrationForm.valid) {
+      if(this.registrationForm.value.password === this.registrationForm.value.repeatpassword){
+        this.buttonState = 'clicked'; 
+        setTimeout(() => { this.buttonState = 'idle'; }, 200); 
+        this.authService.register(registration).subscribe({
+          next: () => {
+            //this.router.navigate(['']);
+          },
+        });
+      } 
+      else{
+        console.log('Passwords do not match!'); // Treba dodati neki vid validacije
+      }
+    }
+    else{
+      console.log('Sign up form not valid!'); // Treba dodati neki vid validacije
+    }
+  }
+
+  togglePasswordVisibility() {
+    this.isPasswordVisible = !this.isPasswordVisible;
+  }
+
+  toggleRepeatPasswordVisibility() {
+    this.isRepeatPasswordVisible = !this.isRepeatPasswordVisible;
+  }
+
+  faEye = faEye;
+  faEyeSlash = faEyeSlash;
+}
