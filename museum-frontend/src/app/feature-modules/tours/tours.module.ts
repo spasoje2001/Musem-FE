dfc32f40acb53f47d571a33e7718fcdb0b2f63e0
dokma11 <vule.dok@gmail.com>
2024-04-11 23:29:17 +0200

    feat: Tours module initialization

---
+++ museum-frontend/src/app/feature-modules/tours/tours.module.ts
@@ -0,0 +1,30 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { MaterialModule } from "src/app/infrastructure/material/material-module";
+import { AdministrationModule } from "../administration/administration.module";
+import { TourViewComponent } from "./tour-view/tour-view.component";
+import { TourCardViewComponent } from './tour-card-view/tour-card-view.component';
+
+@NgModule({
+  declarations: [
+    TourViewComponent,
+    TourCardViewComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule,
+    FontAwesomeModule,  
+    MaterialModule,
+    AdministrationModule
+  ],
+  exports: [
+    TourViewComponent
+  ]
+})
+export class ToursModule { 
+
+}
