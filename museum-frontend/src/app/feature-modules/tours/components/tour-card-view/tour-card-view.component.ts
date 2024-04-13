bb8a5cd1cf8a68f77bf2b3e5a60af51ecba432f5
dokma11 <vule.dok@gmail.com>
2024-04-13 17:41:18 +0200

    style: Tour card style changes

--- museum-frontend/src/app/feature-modules/tours/components/tour-card-view/tour-card-view.component.ts
+++ museum-frontend/src/app/feature-modules/tours/components/tour-card-view/tour-card-view.component.ts
@@ -1,14 +1,30 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
-import { faCoins, faPen, faRemove } from "@fortawesome/free-solid-svg-icons";
+import { faCoins, faPen, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { Tour } from '../../model/tour.model';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'xp-tour-card-view',
   templateUrl: './tour-card-view.component.html',
-  styleUrls: ['./tour-card-view.component.css']
+  styleUrls: ['./tour-card-view.component.css'],
+  animations: [
+    trigger('buttonState', [
+      state('clicked', style({
+        transform: 'scale(0.9)',
+        opacity: 0.5
+      })),
+      transition('* => clicked', [
+        animate('200ms')
+      ]),
+      transition('clicked => idle', [
+        animate('200ms')
+      ])
+    ]),
+  ]
 })
 export class TourCardViewComponent implements OnChanges{
-  
+  editButtonState: string = 'idle';   
+  removeButtonState: string = 'idle'; 
   @Input() tour!: Tour;
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -17,15 +33,17 @@ export class TourCardViewComponent implements OnChanges{
   constructor() {
   }
 
-  edit() {
-
+  editButtonClicked() {
+    this.editButtonState = 'clicked'; 
+    setTimeout(() => { this.editButtonState = 'idle'; }, 200); 
   }
 
-  remove(id: number) {
-
+  removeButtonClicked(id: number) {
+    this.removeButtonState = 'clicked'; 
+    setTimeout(() => { this.removeButtonState = 'idle'; }, 200); 
   }
 
   faCoins = faCoins;
   faPen = faPen;
-  faRemove = faRemove;
+  faRemove = faTrash;
 }
