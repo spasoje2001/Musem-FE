70420f987ab34bdf13fd892271dd89f7738cfa07
dokma11 <vule.dok@gmail.com>
2024-04-15 22:38:18 +0200

    style: Accept personal tour request form style changes

--- museum-frontend/src/app/feature-modules/tours/components/accept-request-form/accept-request-form.component.ts
+++ museum-frontend/src/app/feature-modules/tours/components/accept-request-form/accept-request-form.component.ts
@@ -1,10 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { PersonalTourRequest, PersonalTourRequestStatus } from '../../model/personalTourRequest.model';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ToursService } from '../../tours.service';
+import { DeclineRequestPromptComponent } from '../decline-request-prompt/decline-request-prompt.component';
+import { Curator } from 'src/app/feature-modules/stakeholder/model/curator.model';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-accept-request-form',
   templateUrl: './accept-request-form.component.html',
-  styleUrls: ['./accept-request-form.component.css']
+  styleUrls: ['./accept-request-form.component.css'],
+  animations: [
+      trigger('buttonState', [
+        state('clicked', style({
+          transform: 'scale(0.9)',
+          opacity: 0.5
+        })),
+        transition('* => clicked', [
+          animate('200ms')
+        ]),
+        transition('clicked => idle', [
+          animate('200ms')
+        ])
+      ]),
+  ],
 })
 export class AcceptRequestFormComponent {
+  cancelButtonState: string = 'idle';   
+  acceptButtonState: string = 'idle'; 
+  request: PersonalTourRequest | undefined;
+  curators: Curator[] = [];
+  selectedCurator: Curator | undefined;
+  focused: string = '';
+
+  constructor(private toursService: ToursService,
+              private dialogRef: MatDialogRef<DeclineRequestPromptComponent>,
+              @Inject(MAT_DIALOG_DATA) public data: any) {
+    this.request = data;
+
+    this.toursService.getCurators().subscribe({
+      next: (result: Curator[] | Curator) => {
+        if (Array.isArray(result)) {
+          this.curators = result;
+          console.log(this.curators);
+        }
+      }
+    })
+  }
+
+  acceptRequestForm = new FormGroup({
+    adultTicketPrice: new FormControl('', [Validators.required]),
+    minorTicketPrice: new FormControl('', [Validators.required]),
+  });
+
+  onChooseClicked(curator: Curator){
+    this.selectedCurator = curator;
+  }
+
+  acceptButtonClicked(){
+    this.acceptButtonState = 'clicked'; 
+    setTimeout(() => { this.acceptButtonState = 'idle'; }, 200); 
+
+    this.request!.status = PersonalTourRequestStatus.ACCEPTED;
+    
+    this.toursService.updateTourRequest(this.request!).subscribe({
+      next: () => {
+        this.dialogRef.close();
+      }
+    });
+  }
+
+  cancelButtonClicked(){
+    this.cancelButtonState = 'clicked'; 
+    setTimeout(() => { this.cancelButtonState = 'idle'; }, 200); 
+    this.dialogRef.close();
+  }
+
+  overviewClicked(){
+    this.dialogRef.close();
+  }
 
 }
