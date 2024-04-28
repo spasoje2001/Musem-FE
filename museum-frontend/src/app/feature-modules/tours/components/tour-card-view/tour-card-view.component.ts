304f9c995f87d4e27710c77a61ac0967c6098142
dokma11 <vule.dok@gmail.com>
2024-04-28 15:07:11 +0200

    style: Change tour card content for different roles

--- museum-frontend/src/app/feature-modules/tours/components/tour-card-view/tour-card-view.component.ts
+++ museum-frontend/src/app/feature-modules/tours/components/tour-card-view/tour-card-view.component.ts
@@ -1,5 +1,5 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
-import { faCoins, faPen, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { faCoins, faPen, faTrash, faTicket } from "@fortawesome/free-solid-svg-icons";
 import { Tour } from '../../model/tour.model';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 import { MatDialog } from '@angular/material/dialog';
@@ -8,6 +8,8 @@ import { EditTourFormComponent } from '../edit-tour-form/edit-tour-form.componen
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ToursService } from '../../tours.service';
 import { Curator } from 'src/app/feature-modules/stakeholder/model/curator.model';
+import { AuthService } from 'src/app/infrastructure/auth/auth.service';
+import { User } from 'src/app/infrastructure/auth/model/user.model';
 
 @Component({
   selector: 'xp-tour-card-view',
@@ -31,17 +33,24 @@ import { Curator } from 'src/app/feature-modules/stakeholder/model/curator.model
 export class TourCardViewComponent implements OnInit{
   editButtonState: string = 'idle';   
   removeButtonState: string = 'idle'; 
+  reserveTicketsButtonState: string = 'idle'; 
   @Input() tour!: Tour;
   private dialogRef: any;
   @Output() dialogRefClosed: EventEmitter<any> = new EventEmitter<any>();
+  user: User | undefined;
 
   constructor(private dialog: MatDialog,
               private snackBar: MatSnackBar,
-              private toursService: ToursService) {
+              private toursService: ToursService,
+              private authService: AuthService) {
 
   }
 
   ngOnInit(): void {
+    this.authService.user$.subscribe(user => {
+      this.user = user;
+    });
+
     this.toursService.getCuratorById(this.tour.guideId!).subscribe({
       next: (curator: Curator) => {
         this.tour.guide = curator;
@@ -71,6 +80,11 @@ export class TourCardViewComponent implements OnInit{
     });
   }
 
+  reserveTicketsButtonClicked(id: number){
+    this.reserveTicketsButtonState = 'clicked'; 
+    setTimeout(() => { this.reserveTicketsButtonState = 'idle'; }, 200); 
+  }
+
   showNotification(message: string): void {
     this.snackBar.open(message, 'Close', {
       duration: 3000, 
@@ -82,4 +96,5 @@ export class TourCardViewComponent implements OnInit{
   faCoins = faCoins;
   faPen = faPen;
   faRemove = faTrash;
+  faTicket = faTicket;
 }
