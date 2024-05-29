1040a7544d022ed47379fe25941a52cc942099b1
dokma11 <vule.dok@gmail.com>
2024-05-29 22:36:56 +0200

    feat: Initialize edit tour request form

---
+++ museum-frontend/src/app/feature-modules/tours/components/edit-tour-request-form/edit-tour-request-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-tour-request-form',
+  templateUrl: './edit-tour-request-form.component.html',
+  styleUrls: ['./edit-tour-request-form.component.css']
+})
+export class EditTourRequestFormComponent {
+
+}
