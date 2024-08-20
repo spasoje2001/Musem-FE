b944384c25b0feec2c1bcb839c7ec5b7ceb2c28e
spasoje2001 <spasoje.brboric@gmail.com>
2024-08-20 21:44:07 +0200

    exhibitions view redesign

--- museum-frontend/src/app/feature-modules/exhibitions/exhibitions-view/exhibitions-view.component.ts
+++ museum-frontend/src/app/feature-modules/exhibitions/exhibitions-view/exhibitions-view.component.ts
@@ -13,29 +13,57 @@ export class ExhibitionsViewComponent {
   private dialogRef: any;
   exhibitions: Exhibition[] = [];
   slicedExhibitions: Exhibition[][] = [];
+  activeFilter: string = 'current';
   constructor(private dialog: MatDialog, private exhibitionService: ExhibitionsService){
 
   }
 
   ngOnInit(): void {
+    this.loadExhibitions();
+  }
+
+  loadExhibitions(): void {
     this.exhibitionService.getExhibitions().subscribe({
       next: (result: Exhibition[] | Exhibition) => {
         console.log(result);
-        if(Array.isArray(result)){
-          this.exhibitions = result.filter(
-            (exhibition) => exhibition.status === 'OPEN' || exhibition.status === 'READY_TO_OPEN'
-          );;
-          this.sliceItems();
+        if (Array.isArray(result)) {
+          this.exhibitions = result;
+          this.applyFilter();
         }
       }
-    }); 
-  };
+    });
+  }
 
-  sliceItems() {
+  filterExhibitions(type: string): void {
+    this.activeFilter = type;
+    this.applyFilter();
+  }
+
+  applyFilter(): void {
+    let filteredExhibitions: Exhibition[] = [];
+
+    if (this.activeFilter === 'current') {
+      filteredExhibitions = this.exhibitions.filter(
+        exhibition => exhibition.status === 'OPENED'
+      );
+    } else if (this.activeFilter === 'upcoming') {
+      filteredExhibitions = this.exhibitions.filter(
+        exhibition => exhibition.status === 'READY_TO_OPEN'
+      );
+    } else if (this.activeFilter === 'past') {
+      filteredExhibitions = this.exhibitions.filter(
+        exhibition => exhibition.status === 'CLOSED'
+      );
+    }
+
+    this.sliceItems(filteredExhibitions);
+  }
+
+  sliceItems(filteredExhibitions: Exhibition[]) {
     const itemsPerRow = 4;
     this.slicedExhibitions = [];
-    for (let i = 0; i < this.exhibitions.length; i += itemsPerRow) {
-        this.slicedExhibitions.push(this.exhibitions.slice(i, i + itemsPerRow));
+    for (let i = 0; i < filteredExhibitions.length; i += itemsPerRow) {
+      this.slicedExhibitions.push(filteredExhibitions.slice(i, i + itemsPerRow));
     }
   }
 }
