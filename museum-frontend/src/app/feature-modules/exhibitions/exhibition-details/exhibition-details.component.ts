fa4247cc9893e77a495b4ee69311fa9550335958
spasoje2001 <spasoje.brboric@gmail.com>
2024-08-21 05:34:21 +0200

    prikaz izlozbi i detalji jedne izlozbe, prikaz item-a i detalji jednog itema

--- museum-frontend/src/app/feature-modules/exhibitions/exhibition-details/exhibition-details.component.ts
+++ museum-frontend/src/app/feature-modules/exhibitions/exhibition-details/exhibition-details.component.ts
@@ -11,24 +11,41 @@ import { ExhibitionsService } from '../exhibitions.service';
 export class ExhibitionDetailsComponent {
 
   exhibition!: Exhibition;
+  visibleItems: any[] = [];
+  itemsToShow = 3;
+  selectedItem: any = null;
 
   constructor(
     private route: ActivatedRoute,
     private exhibitionService: ExhibitionsService
   ) {}
 
-  ngOnInit() {
-    // Get the exhibition ID from the route parameters
-    const id = this.route.snapshot.params['id'];
-    // Fetch the exhibition details using the service
-    this.exhibitionService.getExhibitionById(id).subscribe({
-      next: (exhibition: Exhibition) => {
-        this.exhibition = exhibition;
-        console.log(this.exhibition);
-      },
-      error: (err) => {
-        console.error('Error fetching exhibition:', err);
-      }
-    });
-  }
+  
+
+ngOnInit() {
+  const id = this.route.snapshot.params['id'];
+  this.exhibitionService.getExhibitionById(id).subscribe({
+    next: (exhibition: Exhibition) => {
+      this.exhibition = exhibition;
+      this.visibleItems = this.exhibition.itemReservations.slice(0, this.itemsToShow);
+      console.log(this.visibleItems.length)
+    },
+    error: (err) => {
+      console.error('Error fetching exhibition:', err);
+    }
+  });
+}
+
+showMore() {
+  const nextItems = this.exhibition.itemReservations.slice(this.visibleItems.length, this.visibleItems.length + this.itemsToShow);
+  this.visibleItems = [...this.visibleItems, ...nextItems];
+}
+
+showItemDetails(item: any) {
+  this.selectedItem = item;
+}
+
+closeModal() {
+  this.selectedItem = null;
+}
 }
